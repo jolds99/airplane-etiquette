6f76cba140b058cf8189d40f9accfdac52fcfbfd
jolds99 <jonathanolds99@gmail.com>
2020-10-21 15:51:05 -0600

    Adding Tolerance of Baby/Unruly Children EDA

--- Data Exploration.R
+++ Data Exploration.R
@@ -1,9 +1,56 @@
 attach(airplane_etiquette)
 library(tidyverse)
 library(ggplot2)
-#### Obnoxious kids question ####
+library(knitr)
+#### Tolerance of Babies and Children ####
+airplane_etiquette = read_csv("airplane-etiquette.csv")
 
-#### Reclining vs height ####
+# Creating subset of data with complete cases for the three questions
+children = airplane_etiquette[which(!is.na(`Do you have any children under 18?`) &
+                                   !is.na(`In general, is itrude to bring a baby on a plane?`) &
+                                   !is.na(`In general, is it rude to knowingly bring unruly children on a plane?`)),]
+
+# Creating table summarizing counts and proportion of respondents who own children
+childrentable = children %>% group_by(`Do you have any children under 18?`) %>% 
+  summarise(Count = n()) %>% 
+  mutate(Percent = round(Count/sum(Count),3))
+
+kable(childrentable, format = "latex")
+
+# Distribution of responses to baby question
+ggplot(children, aes(x = `In general, is itrude to bring a baby on a plane?`)) + 
+  geom_bar(stat="count", color = "black", fill = "tan") +
+  ggtitle("Distribution of Responses to Baby Question") + 
+  xlab("Responses to \'In general, is it rude to bring a baby on a plane?\'") + 
+  ylab("Frequency") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+# Distribution of responses to children question
+ggplot(children, aes(x = `In general, is it rude to knowingly bring unruly children on a plane?`)) + 
+  geom_bar(stat="count", color = "black", fill = "tan") +
+  ggtitle("Distribution of Responses to Unruly Children Question") + 
+  xlab("Responses to \'In general, is it rude to knowingly bring unruly children on a plane?\'") + 
+  ylab("Frequency") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+babyunruly = children %>% select(RespondentID, `Do you have any children under 18?`, `In general, is itrude to bring a baby on a plane?`,
+                                   `In general, is it rude to knowingly bring unruly children on a plane?`) %>%
+                          gather("Question", "Response", 3:4)
+
+babyunruly$Question = factor(babyunruly$Question, levels = c("In general, is itrude to bring a baby on a plane?",
+                                                                  "In general, is it rude to knowingly bring unruly children on a plane?"))
+ggplot(babyunruly, aes(x = Question, fill = factor(Response))) + 
+         geom_bar(stat = "count", position = position_dodge()) + 
+         ggtitle("Distribution of Baby & Unruly Children Questions") + 
+         labs(fill = "Response") + 
+         ylab("Frequency") + 
+         scale_x_discrete(labels = c("In general, is it rude to bring a baby on a plane?", 
+                                     "In general, is it rude to knowingly bring unruly children on a plane?")) + 
+         scale_fill_manual(values = c("Green4", "Gold2", "Red3")) + 
+         theme(plot.title = element_text(hjust = 0.5))
+
+
+#### Reclining vs Height ####
 nrow(airplane_etiquette)-
   length(which(is.na(`Do you ever recline your seat when you fly?`) | is.na(`How tall are you?`)))
 

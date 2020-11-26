e0a654ec7b697b8e0eff1436d030bfa0248fc991
quinnjohnson303 <quinnjohnson303@gmail.com>
2020-11-25 19:59:02 -0700

    More q4

--- q4.R
+++ q4.R
@@ -8,36 +8,41 @@ library(caret)
 
 #Getting data only necessary for question 4
 q4 <- airplane_etiquette %>%
-  select(., `In a row of three seats, who should get to use the two arm rests?`,
-         `In a row of two seats, who should get to use the middle arm rest?`,
-         Gender, Age, `Location (Census Region)`)
+  mutate(seats3 = `In a row of three seats, who should get to use the two arm rests?`) %>%
+  mutate(seats2 = `In a row of two seats, who should get to use the middle arm rest?`) %>%
+  select(., seats3, seats2, Gender, Age, `Location (Census Region)`)
 #Removing NAs
 q4 <- na.rm(q4)
 
 #Renaming "> 60" to "60+"
 q4$Age <- ifelse(q4$Age == "> 60", "60+", q4$Age)
 
-#Dummy variables
-q4$Gender <- as.factor(q4$Gender)
-q4$Age <- as.factor(q4$Age)
-q4dum <- model.matrix(~ -1 + Age + Gender, data = q4)
-q4dum <- cbind("ThreeSeats" = q4$`In a row of three seats, who should get to use the two arm rests?`,
-            "TwoSeats" = q4$`In a row of two seats, who should get to use the middle arm rest?`,
-            q4dum)
-#Removing Age18-34 to remove collinearity
-q4dum <- as.data.frame(q4dum[,-3])
-
 #Getting levels of responses
-unique(q4$`In a row of three seats, who should get to use the two arm rests?`)
-unique(q4$`In a row of two seats, who should get to use the middle arm rest?`)
+unique(q4$seats3)
+unique(q4$seats2)
 unique(q4$Gender)
 unique(q4$Age)
 
+#Plotting armrest responses
+#2 seats
+ggplot(data = q4, aes(x = seats2, fill = Age)) +
+  geom_bar() +
+  labs(title = "Who should have claim over the middle armrest in a row of two seats?",
+       subtitle = "Separated by Gender", x = "Response", y = "Frequency") +
+  theme_minimal() +
+  facet_grid(~ Gender)
+  
+#3 seats
+ggplot(data = q4, aes(x = seats3, fill = Age)) +
+  geom_bar() +
+  labs(title = "Who should have two armrests in a row of three seats?",
+       subtitle = "Separated by Gender", x = "Response", y = "Frequency") +
+  theme_minimal() +
+  facet_grid(~ Gender)
+
 #Initial multinomial regression test
-twoseat <- multinom(`In a row of two seats, who should get to use the middle arm rest?` ~ Gender + Age, data = q4)
+twoseat <- multinom(seats2 ~ Gender + Age, data = q4)
 summary(twoseat)
-z2 <- summary(twoseat)$coefficients/summary(twoseat)$standard.errors
-p2 <- (1 - pnorm(abs(z2), 0, 1)) * 2; p2
 
 maledf2 <- data.frame(Age = c("18-29", "30-44", "45-60", "60+" ), Gender = "Male")
 malepred2 <- predict(twoseat, newdata = maledf2, "probs")
@@ -53,7 +58,7 @@ ggplot(pred2, aes(x = (Age), y = prob, colour = variable)) +
   facet_grid(Gender ~ ., scales = "free")
 
 
-threeseat <- multinom(`In a row of three seats, who should get to use the two arm rests?` ~ Gender + Age, data = q4)
+threeseat <- multinom(seats3 ~ Gender + Age, data = q4)
 summary(threeseat)
 z3 <- summary(threeseat)$coefficients/summary(threeseat)$standard.errors
 p3 <- (1 - pnorm(abs(z3), 0, 1)) * 2; p3
@@ -69,11 +74,15 @@ pred3 <- melt(pred3, value.name = "prob")
 
 ggplot(pred3, aes(x = (Age), y = prob, colour = variable)) +
   geom_point() +
+  labs(title = "Who should have two armrests in a row of three seats?",
+    subtitle = "Predicted probability that someone in each category would choose each response",
+    x = "Age", y = "Probability of choosing response")+
+  scale_fill_discrete(name = "Response")+
+  theme_minimal()+
   facet_grid(Gender ~ ., scales = "free")
-lm(ThreeSeats ~ . - TwoSeats, data = q4dum)
 
-chisq.test(q4$Age, q4$`In a row of three seats, who should get to use the two arm rests?`)
-chisq.test(q4$Gender, q4$`In a row of three seats, who should get to use the two arm rests?`)
-chisq.test(q4$Age, q4$`In a row of two seats, who should get to use the middle arm rest?`)
-chisq.test(q4$Gender, q4$`In a row of two seats, who should get to use the middle arm rest?`)
+chisq.test(q4$Age, q4$seats3)
+chisq.test(q4$Gender, q4$seats3)
+chisq.test(q4$Age, q4$seats2)
+chisq.test(q4$Gender, q4$seats2)
 

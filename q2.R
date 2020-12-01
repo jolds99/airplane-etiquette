c6c492e5b244d8cc71f657dfaf45c023007004fc
quinnjohnson303 <quinnjohnson303@gmail.com>
2020-12-01 16:21:04 -0700

    Q2

--- q2.R
+++ q2.R
@@ -1,12 +1,13 @@
 library(ggplot2)
 library(rstatix)
 library(nnet)
+library(ggpubr)
+library(car)
 ggplot(q2, aes (x = inches, y = recline)) + geom_boxplot(aes(fill = recline)) + 
   theme_minimal() +
   theme(legend.position = "none") + 
   ylab("Response to 'Do you ever recline your seat when you fly?'") +
   xlab("Height (in.)") +
   labs(title = "Respondents' Height vs Reclining Preferences")
-rec.ttest <- q2 %>% pairwise_t_test(recline ~ inches); rec.ttest
-multinom(recline ~ inches, data = q2)
-kruskal.test(recline ~ inches, data = q2)
+rec.ttest <- q2 %>% pairwise_t_test(inches ~ recline); rec.ttest
+kruskal.test(inches ~ recline, data = q2)
